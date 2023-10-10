2d6f8957395395fff19a5f5dc5a8579e5c1586bb
A5hw1nneg1 <96017158+A5hw1nneg1@users.noreply.github.com>
2023-10-11 00:11:21 +0530

    Add files via upload

---
+++ infopage_turtles.js
@@ -0,0 +1,36 @@
+window.onscroll = function() {myFunction()};
+function myFunction() {
+  if (document.documentElement.scrollTop > 90) {
+    document.getElementById("header").classList.add("scroll_header")
+  } else {
+    document.getElementById("header").classList.add("home_header")
+    document.getElementById("header").classList.remove("scroll_header")
+  }
+}
+
+const type_heading = async () => {
+    let heading = "FIND SEA TURTLES"
+    let letters = heading.split("")
+    let i = 0
+    while (i < letters.length) {
+        await waitForMs(100);
+        document.getElementById("typing-text").append(letters[i])
+        i++
+    }
+    return
+}
+
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+type_heading()
+
+
+document.getElementById("about").addEventListener('click',()=>{
+    document.documentElement.scrollTo(0,750)
+})
+
+document.getElementById("scrolldown").addEventListener('click',()=>{
+    document.documentElement.scrollTo(0,750)
+})
